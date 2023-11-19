bf20f0f30263c8e77c7a7836b8796451ddb0effa
Sumit Maurya <smithdashdash@gmail.com>
2023-11-19 13:08:43 +0530

    Added email and password validator for incoming request

---
+++ src/middlewares/auth-request-validator.js
@@ -0,0 +1,14 @@
+const validateUserAuth =(req,res,next)=>{
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({
+            success:false,
+            data:{},
+            message:"Somthing went wrong",
+            err:'Email or password missing in the signup request'
+        })
+    }
+    next();
+}
+module.exports={
+    validateUserAuth
+}
